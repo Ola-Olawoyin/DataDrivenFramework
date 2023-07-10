a65e1c36f9a8cde41a66aad9ceb6784f6773f0e6
Bosman <oola.olawoyin@gmail.com>
2023-07-10 10:27:07 +0100

    first commit

---
+++ src/test/java/utilities/TestConfig.java
@@ -0,0 +1,35 @@
+package utilities;
+
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "oola.olawoyin@gmail.com";
+	public static String password = "Bosman1707@";
+	public static String[] to ={"bosunolawoxx22@gmail.com","hillde4real@yahoo.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath="C:\\Users\\bosun\\OneDrive\\Pictures\\Colourful Woman.jpg";
+	public static String attachmentName="error.jpg";
+	
+
+	
+	//MYSQL DATABASE DETAILS
+	public static String mysqldriver="com.mysql.cj.jdbc.Driver";
+	public static String mysqluserName = "root";
+	public static String mysqlpassword = "bosman1707";
+	public static String mysqlurl = "jdbc:mysql://127.0.0.1:3306/?user=root/3rdJuly2023";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
